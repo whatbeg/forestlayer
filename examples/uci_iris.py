d4a49dd1dcbc0173ddc9ce5e2522977c5a5d97b6
whatbeg <huqiu00@163.com>
2018-02-08 11:08:15 +0800

    [2018.02.08 11:07] add iris example, minor change of uci_yeast example

--- examples/uci_iris.py
+++ examples/uci_iris.py
@@ -8,7 +8,7 @@ UCI_YEAST Example.
 # License: Apache-2.0
 
 from __future__ import print_function
-from forestlayer.datasets import uci_yeast
+from forestlayer.datasets import uci_iris
 from forestlayer.estimators.estimator_configs import ExtraRandomForestConfig, RandomForestConfig
 from forestlayer.layers.layer import AutoGrowingCascadeLayer
 from forestlayer.layers.graph import Graph
@@ -16,12 +16,12 @@ from forestlayer.utils.storage_utils import get_data_save_base, get_model_save_b
 import os.path as osp
 import time
 
-(x_train, y_train, x_test, y_test) = uci_yeast.load_data()
-
-start_time = time.time()
+(x_train, y_train, x_test, y_test) = uci_iris.load_data()
 
 print('x_train shape: {}'.format(x_train.shape))
 print('x_test.shape: {}'.format(x_test.shape))
+
+start_time = time.time()
 
 est_configs = [
     ExtraRandomForestConfig(),
@@ -34,12 +34,13 @@ est_configs = [
     RandomForestConfig()
 ]
 
-data_save_dir = osp.join(get_data_save_base(), 'uci_yeast')
-model_save_dir = osp.join(get_model_save_base(), 'uci_yeast')
+data_save_dir = osp.join(get_data_save_base(), 'uci_iris')
+model_save_dir = osp.join(get_model_save_base(), 'uci_iris')
 
 auto_cascade = AutoGrowingCascadeLayer(est_configs=est_configs,
                                        early_stopping_rounds=4,
-                                       n_classes=10,
+                                       n_classes=3,
+                                       stop_by_test=False,
                                        data_save_dir=data_save_dir,
                                        model_save_dir=model_save_dir,
                                        distribute=False,
@@ -50,4 +51,3 @@ model.add(auto_cascade)
 model.fit_transform(x_train, y_train, x_test, y_test)
 
 print("time cost: {}".format(time.time() - start_time))
-
